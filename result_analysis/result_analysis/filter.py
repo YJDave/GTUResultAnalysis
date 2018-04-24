d22aa4d9105817c918b4bbbe92dcac4fd8c9f740
YJDave <yashashvidave@gmail.com>
2018-04-24 12:29:13 +0530

    Add new app resultapp.

---
+++ result_analysis/result_analysis/filter.py
@@ -0,0 +1,18 @@
+from .models import Result
+
+def get_overall_result():
+	rs = BranchResult.objects.all()
+	return rs
+
+def get_branch_wise_result():
+	
+
+def get_subject_wise_result():
+
+
+def get_top_students_result():
+
+
+def get_gender_wise_result():
+
+
--- result_analysis/result_analysis/urls.py
+++ result_analysis/result_analysis/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
-from .views import (
+from resultapp.views import (
 	HomePage,
 )
 
