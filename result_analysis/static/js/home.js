978c57813ceb58eb82a1e6a284a6105fa8155e58
YJDave <yashashvidave@gmail.com>
2018-04-24 09:38:20 +0530

    js: Add custom js file.

---
+++ result_analysis/static/js/home.js
@@ -0,0 +1,17 @@
+/*	function dynamicdropdown(listindex)
+{
+    switch (listindex)
+    {
+    case "bs1reg" :
+        document.formname.status.options[0]=new Option("Select status","");
+        document.formname.status.options[1]=new Option("OPEN","open");
+        document.formname.status.options[2]=new Option("DELIVERED","delivered");
+        break;
+    case "bs2reg" :
+			document.formname.status.options[0]=new Option("Select status","");
+			document.formname.status.options[1]=new Option("OPEN","open");
+			document.formname.status.options[2]=new Option("DELIVERED","delivered");
+        break;
+    }
+    return true;
+}*/
